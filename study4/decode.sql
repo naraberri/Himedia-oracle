20cc49a8f2cf2ffcf8146cd7f0e6dac76598da41
naraberri <naraberri@gmail.com>
2023-05-25 17:25:08 +0900

    study4

--- study4/decode.sql
+++ study4/decode.sql
@@ -18,6 +18,7 @@ select
 from emp;
 
 --풀어보기1
+--부서번호 10인 사원은 급여에서 1.1 20인 사원은 1.2 다른부서는 1.3을 인상하여 출력한다.
 select
 empno 사원번호,
 sal 급여,
@@ -25,21 +26,26 @@ decode(deptno,10,sal*1.1,20,sal*1.2,30,sal*1.3,40,sal*1.3) 인상급여
 from emp;
 
 --풀어보기2
+--emp 테이블에서 아래와 같이 설정하여 출력한다.
+--job => ANALYST:분석가,CLERK:점원,MANAGER:관리자,SALESMAN:영업사원,PRESIDENT:사장님
 select 
 e1.*,decode(e2.job,'ANALYST','분석가','CLERK','점원','MANAGER','관리자','SALESMAN','영업사원','PRESIDENT','사장님') 직책
 from emp e1,emp e2 
 where e1.ename = e2.ename order by e1.sal desc;
 
 --풀어보기3
+--사원급여가 2000보다 많으면 상, 그렇지 않으면 하
 select 
 e.*,decode(s.grade,1,'하',2,'하',3,'하',4,'상',5,'상') 등급 
 from emp e,salgrade s 
 where e.sal between s.losal and s.hisal order by e.sal desc;
 
 --풀어보기4
+--job: ANALYST:sal*1.10,CLERK:sal*1.15,MANAGER:sal*1.20
 select 
 ename,sal,nvl(decode(job,'ANALYST',sal*1.10,'CLERK',sal*1.15,'MANAGER',sal*1.20),0) 인상급여 
 from emp;
 
 --풀어보기5
+--1월~6월까지 년도와 상관없이 해당 월에 입사한 사람 수를 출력하시오.
 select count(decode(to_char(hiredate,'mm'),01,1,02,1,03,1,04,1,05,1,06,1)) 입사인원 from emp where to_char(hiredate,'mm')<=6;
